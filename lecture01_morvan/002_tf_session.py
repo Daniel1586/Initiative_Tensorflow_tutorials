dd96d94c708617bfdfb461f1bfd8aef0eec46812
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2018-08-23 20:36:01 +0800

    update session for tensorflow

--- lecture01_morvan/002_tf_session.py
+++ lecture01_morvan/002_tf_session.py
@@ -3,17 +3,17 @@
 
 import tensorflow as tf
 
-matrix1 = tf.constant([[3, 3]])
-matrix2 = tf.constant([[2], [2]])
-product = tf.matmul(matrix1, matrix2)
+matrix1 = tf.constant([[3, 4]])
+matrix2 = tf.constant([[2], [3]])
+dot_op = tf.matmul(matrix1, matrix2)
 
 # method 1
 sess = tf.Session()
-result = sess.run(product)
-print(result)
+result = sess.run(dot_op)
+print('Method 1: ', result)
 sess.close()
 
 # method 2
 with tf.Session() as sess:
-    result2 = sess.run(product)
-    print(result2)
+    result2 = sess.run(dot_op)
+    print('Method 2: ', result2)

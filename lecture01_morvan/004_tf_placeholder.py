aabf1a8ce25314547d88c426c79b986d4ea01fea
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2018-08-23 20:51:04 +0800

    update placeholder for tensorflow

--- lecture01_morvan/004_tf_placeholder.py
+++ lecture01_morvan/004_tf_placeholder.py
@@ -3,9 +3,25 @@
 
 import tensorflow as tf
 
-input1 = tf.placeholder(tf.float32)
-input2 = tf.placeholder(tf.float32)
-output = tf.multiply(input1, input2)
+x1 = tf.placeholder(dtype=tf.float32, shape=None)
+y1 = tf.placeholder(dtype=tf.float32, shape=None)
+z1 = x1 + y1
+
+x2 = tf.placeholder(dtype=tf.float32, shape=[2, 1])
+y2 = tf.placeholder(dtype=tf.float32, shape=[1, 2])
+z2 = tf.matmul(x2, y2)
 
 with tf.Session() as sess:
-    print(sess.run(output, feed_dict={input1: [7.], input2: [2.]}))
+    # when only one operation to run
+    z1_value = sess.run(z1, feed_dict={x1: 1, y1: 2})
+    print('Run one op: ', z1_value)
+
+    # when run multiple operations
+    z1_value, z2_value = sess.run(
+        [z1, z2],       # run them together
+        feed_dict={
+            x1: 1, y1: 2,
+            x2: [[2], [1]], y2: [[1, 3]]
+        })
+    print('Run multi op: ', z1_value)
+    print('Run multi op: ', z2_value)

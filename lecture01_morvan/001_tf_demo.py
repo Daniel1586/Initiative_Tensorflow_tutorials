1fccb848bdf41497631f20f870cd3e0d8d62978d
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2018-08-23 20:30:46 +0800

    update demo for tensorflow

--- lecture01_morvan/001_tf_demo.py
+++ lecture01_morvan/001_tf_demo.py
@@ -9,10 +9,10 @@ x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data*0.1 + 0.3
 
 # create tensorflow start
-Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-biases = tf.Variable(tf.zeros([1]))
+w = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.zeros([1]))
 
-y = Weights*x_data + biases
+y = w*x_data + b
 loss = tf.reduce_mean(tf.square(y - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
@@ -25,4 +25,4 @@ sess.run(init)
 for step in range(201):
     sess.run(train)
     if step % 20 == 0:
-        print(step, sess.run(Weights), sess.run(biases))
+        print(step, sess.run(w), sess.run(b))

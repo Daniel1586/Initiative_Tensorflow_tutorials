d36dab1f1558decb7947843b646ed74258e39bae
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2018-08-23 20:44:54 +0800

    update variable for tensorflow

--- lecture01_morvan/003_tf_variable.py
+++ lecture01_morvan/003_tf_variable.py
@@ -3,15 +3,13 @@
 
 import tensorflow as tf
 
-state = tf.Variable(0, name='counter')
-
-one = tf.constant(1)
-new_value = tf.add(state, one)
-update = tf.assign(state, new_value)
+var = tf.Variable(0, name='counter')
+add_op = tf.add(var, 1)
+update = tf.assign(var, add_op)
 
 init = tf.initialize_all_variables()
 with tf.Session() as sess:
     sess.run(init)
-    for _ in range(3):
+    for _ in range(5):
         sess.run(update)
-        print(sess.run(state))
+        print(sess.run(var))

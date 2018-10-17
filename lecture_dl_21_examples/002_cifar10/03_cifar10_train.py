fb91df55cfa36c469790720b0e7846b7ce233aa9
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2018-10-17 22:37:42 +0800

    update 002_cifar10 for tensorflow

--- lecture_dl_21_examples/002_cifar10/03_cifar10_train.py
+++ lecture_dl_21_examples/002_cifar10/03_cifar10_train.py
@@ -40,8 +40,8 @@ tf.app.flags.DEFINE_integer('log_frequency', 10,
 
 
 def train():
-  """Train CIFAR-10 for a number of steps."""
-  with tf.Graph().as_default():
+    """ Train CIFAR-10 for a number of steps. """
+    with tf.Graph().as_default():
         global_step = tf.train.get_or_create_global_step()
 
         # Get images and labels for CIFAR-10.
@@ -61,35 +61,36 @@ def train():
         # updates the model parameters.
         train_op = cifar10.train(loss, global_step)
 
-    class _LoggerHook(tf.train.SessionRunHook):
-        """Logs loss and runtime."""
+        class _LoggerHook(tf.train.SessionRunHook):
+            """Logs loss and runtime."""
 
-        def begin(self):
-            self._step = -1
-            self._start_time = time.time()
+            def begin(self):
+                self._step = -1
+                self._start_time = time.time()
 
-        def before_run(self, run_context):
-            self._step += 1
-            return tf.train.SessionRunArgs(loss)  # Asks for loss value.
+            def before_run(self, run_context):
+                self._step += 1
+                return tf.train.SessionRunArgs(loss)  # Asks for loss value.
 
-        def after_run(self, run_context, run_values):
-            if self._step % FLAGS.log_frequency == 0:
-                current_time = time.time()
-                duration = current_time - self._start_time
-                self._start_time = current_time
+            def after_run(self, run_context, run_values):
+                if self._step % FLAGS.log_frequency == 0:
+                    current_time = time.time()
+                    duration = current_time - self._start_time
+                    self._start_time = current_time
 
-                loss_value = run_values.results
-                examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
-                sec_per_batch = float(duration / FLAGS.log_frequency)
+                    loss_value = run_values.results
+                    examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
+                    sec_per_batch = float(duration / FLAGS.log_frequency)
 
-                format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                                'sec/batch)')
-                print(format_str % (datetime.now(), self._step, loss_value,
-                                    examples_per_sec, sec_per_batch))
+                    format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                                  'sec/batch)')
+                    print(format_str % (datetime.now(), self._step, loss_value,
+                                        examples_per_sec, sec_per_batch))
 
-    with tf.train.MonitoredTrainingSession(checkpoint_dir=FLAGS.train_dir,
+    with tf.train.MonitoredTrainingSession(
+            checkpoint_dir=FLAGS.train_dir,
             hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
-               tf.train.NanTensorHook(loss), _LoggerHook()],
+                   tf.train.NanTensorHook(loss), _LoggerHook()],
             config=tf.ConfigProto(log_device_placement=FLAGS.log_device_placement)) as mon_sess:
         while not mon_sess.should_stop():
             mon_sess.run(train_op)
@@ -97,6 +98,7 @@ def train():
 
 def main(argv=None):
     cifar10.maybe_download_and_extract()
+    # 重置train_dir;存在则删掉,再创建
     if tf.gfile.Exists(FLAGS.train_dir):
         tf.gfile.DeleteRecursively(FLAGS.train_dir)
     tf.gfile.MakeDirs(FLAGS.train_dir)
